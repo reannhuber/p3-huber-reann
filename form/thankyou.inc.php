bfb3f727f771340c5157aefd8e4dbdadc8b30234
Reann Huber <reannhuberphoto@gmail.com>
2020-12-17 23:07:16 -0600

    Added contact form code

---
+++ form/thankyou.inc.php
@@ -0,0 +1,83 @@
+<style type="text/css">
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+body {
+  background: #d2d6d8;
+}
+
+header {
+  background: #3f0c01;
+  margin-bottom: 10px;
+}
+
+nav {
+  text-align: center;
+  padding: 10px 0;
+}
+
+header nav ul li {
+    display: inline;
+    padding: 30px 15px;
+}
+
+header nav a {
+  color: white;
+  font-family: Montserrat;
+  margin: 0 auto;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: .3s;
+}
+
+header nav a:hover {
+  color: #d8a588;
+  transition: .3s;
+}
+
+h1,
+h2 {
+  color: #a55426;
+  font-family: Montserrat;
+  margin: 0 auto;
+  max-width: 500px;
+  padding-bottom: 10px;
+}
+
+p {
+  font-family: Montserrat Light;
+  padding-bottom: 30px;
+  margin: 0 auto;
+  max-width: 500px;
+}
+
+.top {
+  width: 100%;
+}
+
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="projects.html">Projects</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Thank you for your enquiry</h1>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+</div>
+
+<footer>
+  <p>Website by Reann Huber</p>
+</footer>
